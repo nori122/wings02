831d58341f0795bc348222371af8e04163640324
nori122 <norito.sasaki.jp@gmail.com>
2020-10-22 00:39:28 +0900

    1022_0339_initial commit

---
+++ env.production.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+module.exports = {
+  BASE_URL: 'http://localhost:3000',
+  FB_API_KEY: 'AIzaSyAAbSEmqeBfHyoXseOlyDA2UQexVxoLB1s',
+  FB_AUTH_DOMAIN: 'wings02-acd9a.firebaseapp.com',
+  FB_DATABASE_URL: 'https://wings02-acd9a.firebaseio.com',
+  FB_PROJECTID: 'wings02-acd9a',
+  FB_STORAGE_BUCKET: 'wings02-acd9a.appspot.com',
+  FB_MESSAGING_SENDER_ID: '1095002366967',
+  FB_APP_ID: '1:1095002366967:web:17871b8988950da3ff44f2',
+  FB_MEASUREMENT_ID: 'G-KE8YTZX0XL',
+}
---
+++ plugins/firebase.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase'
+import 'firebase/auth'
+import 'firebase/messaging'
+
+const config = {
+  apiKey: process.env.FB_API_KEY,
+  authDomain: process.env.FB_AUTH_DOMAIN,
+  databaseURL: process.env.FB_DATABASE_URL,
+  projectId: process.env.FB_PROJECTID,
+  storageBucket: process.env.FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+  appId: process.env.FB_APP_ID,
+  timestampsInSnapshots: true,
+}
+if (!firebase.apps.length) {
+  firebase.initializeApp(config)
+}
+
+export const authProviders = {
+  Email: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  Google: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+}
+
+export const auth = firebase.auth()
