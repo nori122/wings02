f021b2d75eaec514c83552b331d89b2aa192ac5b
nori122 <norito.sasaki.jp@gmail.com>
2020-10-26 23:35:05 +0900

    1026_2330_item listをmountedしたい

--- pages/list/index.vue
+++ pages/list/index.vue
@@ -1,19 +1,16 @@
 <template>
   <v-container fluid>
     <v-row justify="center" align="center">
-      <v-col cols="12" sm="8" md="12">
-        <v-btn to="list">List</v-btn>
-      </v-col>
-      <v-col> <list /> </v-col>
+      <v-col> <ItemList /> </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import list from '~/components/list.vue'
+import ItemList from '~/components/ItemList.vue'
 export default {
   components: {
-    list,
+    ItemList,
   },
 }
 </script>
