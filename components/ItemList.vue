917c94dd85b4aa8f45013e99f4a5be63e2add437
nori122 <norito.sasaki.jp@gmail.com>
2020-10-27 19:00:36 +0900

    1027_1900_listの表示項目を修正

--- components/ItemList.vue
+++ components/ItemList.vue
@@ -11,7 +11,6 @@
           <span>翻訳コンテンツの一覧</span>
           <v-btn class="ml-10" @click="getItems">テーブルの更新</v-btn>
           <v-spacer></v-spacer>
-
           <v-text-field
             v-model="search"
             append-icon="mdi-magnify"
@@ -20,8 +19,9 @@
             hide-details
           ></v-text-field>
         </v-card-title>
-        <v-data-table :headers="headers" :items="desserts" :search="search">
-          <template v-slot:item.fat="{ item }">
+
+        <v-data-table :headers="headers" :items="items" :search="search">
+          <template v-slot:item.status="{ item }">
             <v-progress-linear
               :background-opacity="0.3"
               :buffer-value="100"
@@ -30,7 +30,7 @@
               :rounded="true"
               :value="item.statusLevel"
             >
-              <strong>{{ item.status }}</strong>
+              <strong>{{ item.statusDescription }}</strong>
             </v-progress-linear>
           </template>
         </v-data-table>
@@ -51,102 +51,17 @@ export default {
       search: '',
       headers: [
         {
-          text: '最初の項目',
+          text: 'カテゴリ',
           align: 'start',
           sortable: false,
-          value: 'contentUrl',
+          value: 'contentCategory',
         },
-        // { text: 'contentUrl', value: 'contentUrl' },
-        // { text: 'name ', value: 'name' },
-        // { text: 'country ', value: 'country' },
-        // { text: 'createdAt ', value: 'createdAt' },
-        { text: 'Fat (g)', value: 'fat' },
-      ],
-      desserts: [
-        {
-          name: 'test',
-          calories: 159,
-          fat: 6.0,
-          status: '見積もり中',
-          statusLevel: 25,
-          carbs: 24,
-          protein: 4.0,
-          iron: '1%',
-        },
-        {
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          status: '翻訳中',
-          statusLevel: 50,
-          carbs: 37,
-          protein: 4.3,
-          iron: '1%',
-        },
-        {
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: '7%',
-        },
-        {
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: '8%',
-        },
-        {
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: '16%',
-        },
-        {
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: '0%',
-        },
-        {
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: '2%',
-        },
-        {
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: '45%',
-        },
-        {
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: '22%',
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: '6%',
-        },
+        // { text: 'contentTitle ', value: 'contentTitle' },
+        { text: 'タイトル', value: 'contentTitle' },
+        // { text: 'categoryDescription ', value: 'categoryDescription' },
+        { text: 'URL', value: 'contentUrl' },
+        // { text: '作成日 ', value: 'createdAt' },
+        { text: 'status ', value: 'status' },
       ],
     }
   },
