4c95f6b333ec062670efa3548686110dc9f48f6b
nori122 <norito.sasaki.jp@gmail.com>
2020-10-27 18:44:43 +0900

    1027_1844_Register画面でいれるDBの項目を直した

--- components/RegisterItem.vue
+++ components/RegisterItem.vue
@@ -9,34 +9,69 @@
         <v-divider></v-divider>
       </v-col>
       <v-col cols="12" class="ma-5">
-        <div class="text-center text-subtitle-1">
+        <div class="text-subtitle-1">
           既に発信済みのブログの記事のURLから、翻訳を依頼することができます。
         </div>
       </v-col>
-      <v-col cols="12" class="mt-12">
-        <div class="text-center text-subtitle-2">
+      <v-col cols="8" class="mt-12">
+        <div class="text-subtitle-2">
+          翻訳を依頼するコンテンツのカテゴリを選択してください。
+        </div>
+        <v-col cols="12">
+          <v-select
+            :items="category"
+            label="コンテンツのカテゴリ"
+            dense
+            solo
+            v-model="contentCategory"
+          ></v-select>
+        </v-col>
+        <!-- <hr /> -->
+        <div class="text-subtitle-2">
+          コンテンツのカテゴリで「その他」を選択された場合は、文章の種類について簡単に記述ください
+        </div>
+        <v-col cols="12">
+          <v-text-field
+            v-model="categoryDescription"
+            placeholder="（例）Wings Blogsの米国Appストアにおける説明文章"
+            solo
+          ></v-text-field>
+        </v-col>
+        <div class="text-subtitle-2">
+          コンテンツにタイトルをつけてください。このデータは、ご自身の翻訳コンテンツの一覧、検索等で使用されます。
+        </div>
+        <v-col cols="12">
+          <v-text-field
+            v-model="contentTitle"
+            placeholder="（例）海外のユーザーを獲得したい方へ！英語圏の定番プラットフォーム4選"
+            solo
+          ></v-text-field>
+        </v-col>
+        <div class="text-subtitle-2">
           翻訳を依頼するブログ記事を入力してください。
         </div>
+        <v-col cols="12">
+          <v-text-field
+            v-model="contentUrl"
+            placeholder="（例）http://www.yoursite.org/your-post"
+            solo
+          ></v-text-field>
+        </v-col>
       </v-col>
       <v-col cols="12">
-        <v-row justify="center"
-          ><v-col cols="8">
-            <v-text-field
-              v-model="contentUrl"
-              placeholder="http://www.yoursite.org/your-post"
-              solo
-            ></v-text-field> </v-col
-        ></v-row>
-      </v-col>
-
-      <v-col cols="auto" class="mt-12">
-        <v-btn dark width="200px" @click="createItem">インポートする</v-btn>
-      </v-col>
-      <v-col cols="8" offset="4">
-        <div class="text-caption">
-          あなたが所有権を持つ記事のみをインポートしてください
-        </div>
-        <div class="text-caption">次の画面で登録を完了させます。</div>
+        <v-row align="center" justify="center">
+          <v-col cols="auto" class="mt-5">
+            <v-btn dark width="200px" @click="createItem">インポートする</v-btn>
+          </v-col>
+          <v-col cols="12">
+            <div class="text-center text-caption">
+              あなたが所有権を持つ記事のみをインポートしてください
+            </div>
+            <div class="text-center text-caption">
+              次の画面で登録を完了させます。
+            </div>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
@@ -48,6 +83,16 @@ import { db } from '~/plugins/firebase'
 
 export default {
   data: () => ({
+    category: [
+      'ブログ記事',
+      'ホームページ',
+      'Webアプリ',
+      'ネイティブアプリ',
+      'その他（以下に詳細を記載）',
+    ],
+    contentCategory: '',
+    categoryDescription: '',
+    contentTitle: '',
     contentUrl: '',
   }),
   computed: {
@@ -59,8 +104,9 @@ export default {
         .doc(this.uid)
         .collection('items')
         .add({
-          name: 'Tokyo',
-          country: 'Japan',
+          contentCategory: this.contentCategory,
+          categoryDescription: this.categoryDescription,
+          contentTitle: this.contentTitle,
           contentUrl: this.contentUrl,
           createdAt: firebase.firestore.FieldValue.serverTimestamp(),
         })
