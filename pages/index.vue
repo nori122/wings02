677b39cfbfa948e5810e083bec289eda6adfcf97
nori122 <norito.sasaki.jp@gmail.com>
2020-11-16 20:17:25 +0900

    1116_ページ遷移のチラつきを暫定的に解消

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,19 @@
 <template>
   <v-container fluid>
-    <v-row justify="center" align="center">
-      <v-col cols="12">
-        <p class="text-center text-h4">indexページ</p>
+    <!-- <v-row justify="center" align="center">
+      <v-col class="mt-10" cols="auto">
+        <v-progress-circular
+          :size="100"
+          :width="7"
+          color="blue"
+          indeterminate
+        ></v-progress-circular>
       </v-col>
-      <v-col cols="auto">
-        <v-btn width="120px" to="list">List</v-btn>
-      </v-col>
-      <v-col cols="auto">
-        <v-btn width="120px" to="register">register</v-btn>
-      </v-col>
-      <v-col cols="auto">
-        <v-btn width="120px" to="confirm">confirm</v-btn>
-      </v-col>
-    </v-row>
+    </v-row> -->
   </v-container>
 </template>
 <script>
 export default {
-  auth: false,
+  layout: 'landing',
 }
 </script>
